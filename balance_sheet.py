b144eabc12931347088c0fe51beb8ae8b7a330d0
Zane Hutchison <zanehutcho99@gmail.com>
2020-10-10 20:40:36 +1000

    Created a function for finding the data.

--- balance_sheet.py
+++ balance_sheet.py
@@ -1,6 +1,7 @@
 from lxml import html
 from bs4 import BeautifulSoup
 import requests
+import re
 
 BASE_URL = [
     'https://www.reuters.com/companies/2408.T/financials/balance-sheet-quarterly',
@@ -10,27 +11,28 @@ BASE_URL = [
 
 
 def clean_data(data):
-    data = data.strip().replace(",", "")
+    data = data.strip().replace(',', '')
+    data = re.sub("[^0123456789.]", '', data)
     data = float(data)
+    return data
+
+
+def find_data(keyword):
+    data_pos = results.find(keyword)
+    data_splice = results[data_pos:data_pos + 250].split(">")[3]
+    data = clean_data(data_splice)
     return data
 
 
 for url in BASE_URL:
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    results = soup.find(id='__next')
+    results = soup.find(id='__next').prettify()
 
-    cash_and_eq_position = results.prettify().find("Cash &amp; Equivalents") + 188
-    cash_and_eq = clean_data(results.prettify(
-    )[cash_and_eq_position: cash_and_eq_position + 11])
-    print(cash_and_eq)
+    cash_and_equivalents = find_data("Cash &amp; Equivalents")
+    total_current_assets = find_data("Total Current Assets")
+    total_liabilities = find_data("Total Liabilities")
 
-    total_ca_position = results.prettify().find("Total Current Assets") + 186
-    total_ca = clean_data(results.prettify(
-    )[total_ca_position: total_ca_position + 11])
-    print(total_ca)
-
-    total_liab_position = results.prettify().find("Total Liabilities") + 184
-    total_liab = clean_data(results.prettify(
-    )[total_liab_position: total_liab_position + 11])
-    print(total_liab)
+    print("Cash & Equivalents: " + str(cash_and_equivalents))
+    print("Total Current Assets: " + str(total_current_assets))
+    print("Total Liabilities: " + str(total_liabilities))
