03a76b2725ba5b6952bbb6a9540aa041619da006
Zane Hutchison <zanehutcho99@gmail.com>
2020-10-11 08:07:21 +1000

    Added Information Class and calculated values.

--- scraper.py
+++ scraper.py
@@ -6,6 +6,49 @@ import re
 ticker = "9885.T"
 
 
+class Information():
+    results = ""
+    shares_out = 0.0
+    market_cap = 0.0
+
+    def __init__(self, ticker):
+        URL = 'https://www.reuters.com/companies/' + \
+            ticker + '/profile'
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        self.set_results(soup.find(id='__next').prettify())
+
+        self.set_shares_out(self.find_data("Shares Out (MIL)"))
+        self.set_market_cap(self.find_data("Market Cap (MIL)"))
+
+    def clean_string(self, string):
+        data = string.strip().replace(',', '')
+        data = re.sub("[^0123456789.]", '', data)
+        data = float(data)
+        return data
+
+    def find_data(self, keyword):
+        data_pos = self.results.find(keyword)
+        data_splice = self.results[data_pos:data_pos + 350].split(">")[4]
+        data = self.clean_string(data_splice)
+        return data
+
+    def set_results(self, results):
+        self.results = results
+
+    def set_shares_out(self, value):
+        self.shares_out = value
+
+    def set_market_cap(self, value):
+        self.market_cap = value
+
+    def get_shares_out(self):
+        return self.shares_out
+
+    def get_market_cap(self):
+        return self.market_cap
+
+
 class IncomeStatement():
     results = ""
     net_income = 0.0
@@ -38,7 +81,7 @@ class IncomeStatement():
         self.net_income = value
 
     def get_net_income(self):
-        return str(self.net_income)
+        return self.net_income
 
 
 class BalanceSheet():
@@ -83,20 +126,34 @@ class BalanceSheet():
         self.t_liabilities = value
 
     def get_cash_and_eq(self):
-        return str(self.cash_and_eq)
+        return self.cash_and_eq
 
     def get_t_current_assets(self):
-        return str(self.t_current_assets)
+        return self.t_current_assets
 
     def get_t_liabilities(self):
-        return str(self.t_liabilities)
+        return self.t_liabilities
 
 
+ticker_information = Information(ticker)
 ticker_income_statement = IncomeStatement(ticker)
 ticker_balance_sheet = BalanceSheet(ticker)
 
-print("Net Income: " + ticker_income_statement.get_net_income())
+shares_out = ticker_information.get_shares_out()
+market_cap = ticker_information.get_market_cap()
 
-print("Cash & Equivalents: " + ticker_balance_sheet.get_cash_and_eq())
-print("Total Current Assets: " + ticker_balance_sheet.get_t_current_assets())
-print("Total Liabilities: " + ticker_balance_sheet.get_t_liabilities())
+net_income = ticker_income_statement.get_net_income()
+
+cash_and_eq = ticker_balance_sheet.get_cash_and_eq()
+t_current_assets = ticker_balance_sheet.get_t_current_assets()
+t_liabilities = ticker_balance_sheet.get_t_liabilities()
+
+price = market_cap / shares_out
+
+net_current_assets = t_current_assets - t_liabilities
+net_cash = cash_and_eq - t_liabilities
+
+price_to_earnings = market_cap / net_income
+price_to_nca = market_cap / net_current_assets
+price_to_net_cash = market_cap / net_cash
+current_ratio = t_current_assets / t_liabilities
