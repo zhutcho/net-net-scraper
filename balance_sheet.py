5b256ba156f6a7177d8877c8fa0d6d844b66eb52
Zane Hutchison <zanehutcho99@gmail.com>
2020-10-10 10:16:13 +1000

    Created a cleaning function.

--- balance_sheet.py
+++ balance_sheet.py
@@ -8,22 +8,29 @@ BASE_URL = [
     'https://www.reuters.com/companies/9885.T/financials/balance-sheet-quarterly'
 ]
 
+
+def clean_data(data):
+    data = data.strip().replace(",", "")
+    data = float(data)
+    return data
+
+
 for url in BASE_URL:
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     results = soup.find(id='__next')
 
     cash_and_eq_position = results.prettify().find("Cash &amp; Equivalents") + 188
-    cash_and_eq = results.prettify(
-    )[cash_and_eq_position: cash_and_eq_position + 11].strip().replace(",", "")
+    cash_and_eq = clean_data(results.prettify(
+    )[cash_and_eq_position: cash_and_eq_position + 11])
     print(cash_and_eq)
 
     total_ca_position = results.prettify().find("Total Current Assets") + 186
-    total_ca = results.prettify(
-    )[total_ca_position: total_ca_position + 11].strip().replace(",", "")
+    total_ca = clean_data(results.prettify(
+    )[total_ca_position: total_ca_position + 11])
     print(total_ca)
 
     total_liab_position = results.prettify().find("Total Liabilities") + 184
-    total_liab = results.prettify(
-    )[total_liab_position: total_liab_position + 11].strip().replace(",", "")
+    total_liab = clean_data(results.prettify(
+    )[total_liab_position: total_liab_position + 11])
     print(total_liab)
