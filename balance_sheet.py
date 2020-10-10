bb6ee88a7058850cd9501596ae35325c07866960
Zane Hutchison <zanehutcho99@gmail.com>
2020-10-10 10:12:17 +1000

    Pulling specific financial data.

--- balance_sheet.py
+++ balance_sheet.py
@@ -10,5 +10,20 @@ BASE_URL = [
 
 for url in BASE_URL:
     page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    print(soup)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find(id='__next')
+
+    cash_and_eq_position = results.prettify().find("Cash &amp; Equivalents") + 188
+    cash_and_eq = results.prettify(
+    )[cash_and_eq_position: cash_and_eq_position + 11].strip().replace(",", "")
+    print(cash_and_eq)
+
+    total_ca_position = results.prettify().find("Total Current Assets") + 186
+    total_ca = results.prettify(
+    )[total_ca_position: total_ca_position + 11].strip().replace(",", "")
+    print(total_ca)
+
+    total_liab_position = results.prettify().find("Total Liabilities") + 184
+    total_liab = results.prettify(
+    )[total_liab_position: total_liab_position + 11].strip().replace(",", "")
+    print(total_liab)
