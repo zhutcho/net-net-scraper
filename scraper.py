86533833dc4a66f0fc3cba70e39d699033f58943
Zane Hutchison <zanehutcho99@gmail.com>
2020-10-12 13:11:06 +1000

    Removed IncomeStatement, pe scraped from Information.

--- scraper.py
+++ scraper.py
@@ -12,7 +12,7 @@ from time import sleep
 gecko_install = GeckoDriverManager().install()
 driver = webdriver.Firefox(executable_path=gecko_install)
 driver2 = webdriver.Firefox(executable_path=gecko_install)
-driver3 = webdriver.Firefox(executable_path=gecko_install)
+# driver3 = webdriver.Firefox(executable_path=gecko_install)
 
 
 def clean_string(string):
@@ -29,13 +29,16 @@ def find_data(results, keyword, index):
     data_pos = results.find(keyword)
     data_replace = results[data_pos:data_pos + 1000].replace('<', '>')
     data_splice = data_replace.split('>')[index]
-    data = clean_string(data_splice)
+    data = data_splice
+    if keyword != "Price To Earnings (TTM)":
+        data = clean_string(data_splice)
     return data
 
 
 class Information():
     results = ""
     price = 0.0
+    pe_ttm = 0.0
     shares_out = 0.0
     market_cap = 0.0
 
@@ -46,6 +49,7 @@ class Information():
         self.set_results(soup.find(id='__next').prettify())
 
         self.set_price(find_data(self.results, "Latest Trade", 4))
+        self.set_pe_ttm(find_data(self.results, "Price To Earnings (TTM)", 8))
         self.set_shares_out(find_data(self.results, "Shares Out (MIL)", 8))
         self.set_market_cap(find_data(self.results, "Market Cap (MIL)", 8))
 
@@ -57,6 +61,12 @@ class Information():
 
     def set_price(self, value):
         self.price = value
+
+    def get_pe_ttm(self):
+        return self.pe_ttm
+
+    def set_pe_ttm(self, value):
+        self.pe_ttm = value
 
     def get_shares_out(self):
         return self.shares_out
@@ -71,30 +81,6 @@ class Information():
         self.market_cap = value
 
 
-class IncomeStatement():
-    results = ""
-    net_income = 0.0
-
-    def __init__(self, ticker):
-        URL = 'https://www.reuters.com/companies/' + \
-            ticker + '/financials/income-statement-annual'
-        driver2.get(URL)
-        soup = BeautifulSoup(driver2.page_source, 'html.parser')
-        self.set_results(soup.find(id='__next').prettify())
-
-        self.set_net_income(
-            find_data(self.results, "Diluted Net Income", 6))
-
-    def set_results(self, results):
-        self.results = results
-
-    def get_net_income(self):
-        return self.net_income
-
-    def set_net_income(self, value):
-        self.net_income = value
-
-
 class BalanceSheet():
     results = ""
     date = ""
@@ -105,8 +91,8 @@ class BalanceSheet():
     def __init__(self, ticker):
         URL = 'https://www.reuters.com/companies/' + \
             ticker + '/financials/balance-sheet-quarterly'
-        driver3.get(URL)
-        soup = BeautifulSoup(driver3.page_source, 'html.parser')
+        driver2.get(URL)
+        soup = BeautifulSoup(driver2.page_source, 'html.parser')
         self.set_results(soup.find(id='__next').prettify())
 
         self.set_cash_and_eq(
@@ -151,20 +137,16 @@ def to_CSV():
     ticker_list = get_list()
     with open('net_net_data.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(["ticker"] + ["pe"] + ["price to nca"] +
+        writer.writerow(["ticker"] + ["pe (ttm)"] + ["price to nca"] +
                         ["price to net cash"] + ["current ratio"])
         for ticker in ticker_list:
             print(ticker)
             ticker_information = Information(ticker)
-            ticker_income_statement = IncomeStatement(ticker)
             ticker_balance_sheet = BalanceSheet(ticker)
-            sleep(random.randint(2, 10))
 
             price = ticker_information.get_price()
             shares_out = ticker_information.get_shares_out()
             market_cap = ticker_information.get_market_cap()
-
-            net_income = ticker_income_statement.get_net_income()
 
             cash_and_eq = ticker_balance_sheet.get_cash_and_eq()
             t_current_assets = ticker_balance_sheet.get_t_current_assets()
@@ -173,11 +155,11 @@ def to_CSV():
             net_current_assets = t_current_assets - t_liabilities
             net_cash = cash_and_eq - t_liabilities
 
-            price_to_earnings = round(market_cap / net_income, 2)
+            pe_ttm = ticker_information.get_pe_ttm()
             price_to_nca = round(market_cap / net_current_assets, 2)
             price_to_net_cash = round(market_cap / net_cash, 2)
             current_ratio = round(t_current_assets / t_liabilities, 2)
-            writer.writerow([ticker] + [price_to_earnings] +
+            writer.writerow([ticker] + [pe_ttm] +
                             [price_to_nca] + [price_to_net_cash] + [current_ratio])
 
 
